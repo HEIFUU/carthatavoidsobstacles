a3783fb1589133f24442c21885ed5925d20b9b2e
kadirhan <139798028+heifuu@users.noreply.github.com>
2023-08-05 12:22:53 +0300

    Update main.ino

--- main.ino
+++ main.ino
@@ -1,112 +1,169 @@
- #include <Servo.h>
- Servo servo1;
- Servo servo2;
- Servo servo3;
-  const int motorA1  = 8;  // L298N IN3
-  const int motorA2  = 6;  // L298N IN1 
-  const int motorB1  = 7; // L298N IN2 
-  const int motorB2  = 9;  // L298N IN4 
-  int i=0; 
-  int j=0; 
-  int state; 
-  int vSpeed=255;
+
+#include <AFMotor.h>  
+#include <NewPing.h>
+#include <Servo.h> 
+
+#define TRIG_PIN A0 
+#define ECHO_PIN A1 
+#define MAX_DISTANCE 200 
+#define MAX_SPEED 190 // sets speed of DC  motors
+#define MAX_SPEED_OFFSET 20
+
+NewPing sonar(TRIG_PIN, ECHO_PIN, MAX_DISTANCE); 
+
+AF_DCMotor motor1(1, MOTOR12_1KHZ); 
+AF_DCMotor motor2(2, MOTOR12_1KHZ);
+AF_DCMotor motor3(3, MOTOR34_1KHZ);
+AF_DCMotor motor4(4, MOTOR34_1KHZ);
+Servo myservo;   
+
+boolean goesForward=false;
+int distance = 100;
+int speedSet = 0;
 
 void setup() {
-    pinMode(motorA1, OUTPUT);
-    pinMode(motorA2, OUTPUT);
-    pinMode(motorB1, OUTPUT);
-    pinMode(motorB2, OUTPUT); 
-    servo1.attach(2);
-    servo2.attach(3);
-    servo3.attach(5);  
-    Serial.begin(9600);
+
+  myservo.attach(10);  
+  myservo.write(115); 
+  delay(2000);
+  distance = readPing();
+  delay(100);
+  distance = readPing();
+  delay(100);
+  distance = readPing();
+  delay(100);
+  distance = readPing();
+  delay(100);
 }
- 
+
 void loop() {
-    if(Serial.available() > 0){     
-      state = Serial.read();   
-    }  
-/* 4 Speed Levels */      
-    if (state == '0'){
-      vSpeed=0;}
-    else if (state == '1'){
-      vSpeed=100;}
-    else if (state == '2'){
-      vSpeed=180;}
-    else if (state == '3'){
-      vSpeed=200;}
-    else if (state == '4'){
-      vSpeed=255;}
-     
-  /***********************FORWARD****************************/
-    if (state == 'F') {
-      analogWrite(motorA1, vSpeed); analogWrite(motorA2, 0);
-        analogWrite(motorB1, vSpeed);      analogWrite(motorB2, 0); 
-    }
-  /**********************F-LEFT************************/
-    else if (state == 'G') {
-      analogWrite(motorA1,vSpeed ); analogWrite(motorA2, 0);  
-        analogWrite(motorB1, 100);    analogWrite(motorB2, 0); 
-    }
-  /**********************F-RIGHT************************/
-    else if (state == 'I') {
-        analogWrite(motorA1, 100); analogWrite(motorA2, 0); 
-        analogWrite(motorB1, vSpeed);      analogWrite(motorB2, 0); 
-    }
-  /***********************BACK****************************/
-    else if (state == 'B') {
-      analogWrite(motorA1, 0);   analogWrite(motorA2, vSpeed); 
-        analogWrite(motorB1, 0);   analogWrite(motorB2, vSpeed); 
-    }
-  /**********************B-LEFT************************/
-    else if (state == 'H') {
-      analogWrite(motorA1, 0);   analogWrite(motorA2, 100); 
-        analogWrite(motorB1, 0); analogWrite(motorB2, vSpeed); 
-    }
-  /**********************B-RIGHT************************/
-    else if (state == 'J') {
-      analogWrite(motorA1, 0);   analogWrite(motorA2, vSpeed); 
-        analogWrite(motorB1, 0);   analogWrite(motorB2, 100); 
-    }
-  /***************************LEFT*****************************/
-    else if (state == 'L') {
-      analogWrite(motorA1, vSpeed);   analogWrite(motorA2, 150); 
-        analogWrite(motorB1, 0); analogWrite(motorB2, 0); 
-    }
-  /***************************RIGHT*****************************/
-    else if (state == 'R') {
-      analogWrite(motorA1, 0);   analogWrite(motorA2, 0); 
-        analogWrite(motorB1, vSpeed);   analogWrite(motorB2, 150);     
-    }
-  
-  /************************STOP*****************************/
-    else if (state == 'S'){
-        analogWrite(motorA1, 0);  analogWrite(motorA2, 0); 
-        analogWrite(motorB1, 0);  analogWrite(motorB2, 0);
-    } 
+ int distanceR = 0;
+ int distanceL =  0;
+ delay(40);
+ 
+ if(distance<=15)
+ {
+  moveStop();
+  delay(100);
+  moveBackward();
+  delay(300);
+  moveStop();
+  delay(200);
+  distanceR = lookRight();
+  delay(200);
+  distanceL = lookLeft();
+  delay(200);
 
-    /********************SERVO-COMMANDS******************/
-
-    else if (state == 'W'){
-        servo1.write(0);
-        servo2.write(160);
-    }
-    else if (state == 'U'){
-        servo1.write(30);
-        servo2.write(130);
-    }
-    else if (state == 'V'){
-        servo1.write(90);
-        servo2.write(70);
-    }
-    else if (state == 'X'){
-        servo1.write(160);
-        servo2.write(0);
-    }
-    else if (state == 'Y'){
-      servo3.write(0);
-      delay(250);
-      servo3.write(75);
-      delay(250);
-    }
+  if(distanceR>=distanceL)
+  {
+    turnRight();
+    moveStop();
+  }else
+  {
+    turnLeft();
+    moveStop();
+  }
+ }else
+ {
+  moveForward();
+ }
+ distance = readPing();
 }
+
+int lookRight()
+{
+    myservo.write(50); 
+    delay(500);
+    int distance = readPing();
+    delay(100);
+    myservo.write(115); 
+    return distance;
+}
+
+int lookLeft()
+{
+    myservo.write(170); 
+    delay(500);
+    int distance = readPing();
+    delay(100);
+    myservo.write(115); 
+    return distance;
+    delay(100);
+}
+
+int readPing() { 
+  delay(70);
+  int cm = sonar.ping_cm();
+  if(cm==0)
+  {
+    cm = 250;
+  }
+  return cm;
+}
+
+void moveStop() {
+  motor1.run(RELEASE); 
+  motor2.run(RELEASE);
+  motor3.run(RELEASE);
+  motor4.run(RELEASE);
+  } 
+  
+void moveForward() {
+
+ if(!goesForward)
+  {
+    goesForward=true;
+    motor1.run(FORWARD);      
+    motor2.run(FORWARD);
+    motor3.run(FORWARD); 
+    motor4.run(FORWARD);     
+   for (speedSet = 0; speedSet < MAX_SPEED; speedSet +=2) 
+   {
+    motor1.setSpeed(speedSet);
+    motor2.setSpeed(speedSet);
+    motor3.setSpeed(speedSet);
+    motor4.setSpeed(speedSet);
+    delay(5);
+   }
+  }
+}
+
+void moveBackward() {
+    goesForward=false;
+    motor1.run(BACKWARD);      
+    motor2.run(BACKWARD);
+    motor3.run(BACKWARD);
+    motor4.run(BACKWARD);  
+  for (speedSet = 0; speedSet < MAX_SPEED; speedSet +=2) 
+  {
+    motor1.setSpeed(speedSet);
+    motor2.setSpeed(speedSet);
+    motor3.setSpeed(speedSet);
+    motor4.setSpeed(speedSet);
+    delay(5);
+  }
+}  
+
+void turnRight() {
+  motor1.run(FORWARD);
+  motor2.run(FORWARD);
+  motor3.run(BACKWARD);
+  motor4.run(BACKWARD);     
+  delay(500);
+  motor1.run(FORWARD);      
+  motor2.run(FORWARD);
+  motor3.run(FORWARD);
+  motor4.run(FORWARD);      
+} 
+ 
+void turnLeft() {
+  motor1.run(BACKWARD);     
+  motor2.run(BACKWARD);  
+  motor3.run(FORWARD);
+  motor4.run(FORWARD);   
+  delay(500);
+  motor1.run(FORWARD);     
+  motor2.run(FORWARD);
+  motor3.run(FORWARD);
+  motor4.run(FORWARD);
+}  
